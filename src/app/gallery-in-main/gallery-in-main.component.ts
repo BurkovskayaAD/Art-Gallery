fcdadf14e09087ba4c4f190db2cbe573e33a17ca
Burkouskaja <hanna.burkovskaya@gmail.com>
2020-10-09 13:48:22 +0300

    Updated photos and started fix gallery-in-main copmonent.

--- src/app/gallery-in-main/gallery-in-main.component.ts
+++ src/app/gallery-in-main/gallery-in-main.component.ts
@@ -12,27 +12,16 @@ export class GalleryInMainComponent implements OnInit {
   public list = [];
   public url = [];
 
-  // tslint:disable-next-line:typedef
-  public ngOnInit(){
+
+  ngOnInit(): void{
     this.list = [
-      {rows : 2, cols : 1, data : 1},
-      {rows : 1, cols : 1, data : 2},
-      {rows : 2, cols : 1, data : 3},
-      {rows : 1, cols : 1, data : 4},
-      {rows : 1, cols : 1, data : 5},
-      {rows : 1, cols : 1, data : 6},
-    ];
-    this.url = [
-      'assets/5.jpg',
-      'assets/14.jpg',
-      'assets/13.jpg',
-      'assets/12.jpg',
-      'assets/8.jpg',
-      'assets/7.jpg'
+      {rows : 2, cols : 1, url : 'assets/14.jpg'},
+      {rows : 1, cols : 1, url : 'assets/10.jpeg'},
+      {rows : 2, cols : 1, url : 'assets/13.jpg'},
+      {rows : 1, cols : 1, url : 'assets/11.jpeg'},
+      {rows : 1, cols : 1, url : 'assets/5.jpg'},
+      {rows : 1, cols : 1, url : 'assets/7.jpg'},
     ];
   }
-
-  // ngOnInit(): void {
-  // }
 
 }
