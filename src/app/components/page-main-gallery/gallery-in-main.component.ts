aeefd30d149f4aaf961a1bd211f6d9a2cb36f812
BurkovskayaAD <hanna.burkovskaya@gmail.com>
2020-12-09 09:49:37 +0300

    Work with structure in gallery-in-main

--- src/app/components/page-main-gallery/gallery-in-main.component.ts
+++ src/app/components/page-main-gallery/gallery-in-main.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 import {HttpServicesService} from '../../services/http-services.service';
-import { Constants } from '../../Constants';
+import {Constants} from '../../Constants';
 
 @Component({
   selector: 'app-gallery-in-main',
@@ -10,7 +10,8 @@ import { Constants } from '../../Constants';
 })
 export class GalleryInMainComponent implements OnInit {
 
-  constructor(private http: HttpServicesService) { }
+  constructor(private http: HttpServicesService) {
+  }
 
   painting;
   picture;
@@ -19,35 +20,30 @@ export class GalleryInMainComponent implements OnInit {
   public url = [];
 
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.http.get(Constants.paintingsApiUrl).subscribe(
       (painting) => {
         this.painting = painting;
         console.log(this.painting);
 
-        // for (let i = 0; i < this.painting.length; i++){
-        //   this.painting[i].huge = false;
-        // }
-        // for (let i = 0; i < this.painting.length; i = i + 3){
-        //   this.painting[i].huge = true;
-        // }
-        // for (let i = 1; i < this.painting.length; i = i + 3){
-        //   this.painting[i].url1 = this.painting[i].picture;
-        // }
-        // for (let i = 2; i < this.painting.length; i = i + 3){
-        //   this.painting[i].url2 = this.painting[i].picture;
-        // }
-
         this.painting[0].huge = true;
         this.painting[3].huge = true;
 
+        this.painting[1].url1 = this.painting[1].picture;
+        this.painting[2].url2 = this.painting[2].picture;
+        this.painting[4].url1 = this.painting[4].picture;
+
         this.list = [
           this.painting[0],
-          [ this.painting[1], this.painting[2] ],
+          [this.painting[1], this.painting[2]],
           this.painting[3],
-          [ this.painting[4] ]
+          [this.painting[4]]
         ];
 
+        this.list[1].huge = false;
+        this.list[3].huge = false;
+
+        console.log(this.list);
 
         // this.list = [
         //   {huge: true, url: 'assets/14.jpg'},
@@ -64,6 +60,6 @@ export class GalleryInMainComponent implements OnInit {
         //   },
         // ];
       }
-      );
-      }
+    );
+  }
 }
